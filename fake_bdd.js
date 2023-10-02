915beadcef47e1a716023047058035a0375e007e
karl722 <140514499+karl722@users.noreply.github.com>
2023-10-02 15:58:18 +0200

    Add files via upload

---
+++ fake_bdd.js
@@ -0,0 +1,84 @@
+var article1={
+    titre:"T-Shirt Rouge garçon",
+    description: "T-Shirt manche courte rouge, encolure chemise avec 2 boutons.",
+    photo: "tee_shirt_garcon.jpg",
+    prix:6
+  
+  }
+  
+  var article2={
+    titre:"Pull gris garçon",
+    description:"Pull gris manche longue, encolure ronde, pull en jersey pur coton",
+    photo:"pull_garcon.jpg",
+    prix:10
+  
+  
+  }
+  
+  var article3={
+    titre:"Pantalon jean noir garçon",
+    description:"Taille ajustable de l'intérieur par élastique et boutons,en coton et polyester",
+    photo:"pantalon_garcon.jpg",
+    prix:15
+  
+  }
+  
+  var article4={
+    titre:"Manteau garçon",
+    description:"Manteau marron, col aviateur, en simili-cuir.",
+    photo:"images (2).jpeg",
+    prix:40
+  }
+  
+  var article5={
+    titre:"Baskets garçon",
+    description:"Baskets à lacets vert anis et blanche.",
+    photo:"images.jpeg",
+    prix:25
+  }
+  
+  var article6={
+    titre:"T-Shirt fille",
+    description: "T-Shirt manche courte rayé rose et blanc, col rond.100% coton",
+    photo: "images (3).jpeg",
+    prix:6
+  
+  
+  }
+  
+  var article7={
+    titre:"Pull fille",
+    description:"Pull blanc, manche longue, col roulé, pull en laine",
+    photo:"pull_fille.jpg",
+    prix:10
+  
+  
+  }
+  
+  var article8={
+    titre:"Pantalon jean noir fille",
+    description:"Taille ajustable de l'intérieur par élastique et boutons,en coton et polyester",
+    photo:"pantalon_fille.jpg",
+    prix:10
+  
+  
+  }
+  
+  var article9={
+    titre:"Manteau fille",
+    description:"Manteau rouge,corps doublé maille peluche, manches et corps ouatinés.Ouverture zippée sous patte avec brandebourgs.",
+    photo:"manteau_fille.jpg",
+    prix:40
+  
+  
+  }
+  
+  var article10={
+    titre:"Baskets fille",
+    description:"Baskets à lacets rose et blanche.",
+    photo:"images (1).jpeg",
+    prix:25
+  
+  
+  }
+  var catalogue = [article1, article2, article3,article4,article5,article6,article7,article8,article9,article10];
